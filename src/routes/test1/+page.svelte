548b29e9c8002b1b871572b18855a261216baa35
Ondřej Čížek <ondrej.cizek@giant.cz>
2023-08-07 21:09:42 +0200

    svelte:window bindings

--- src/routes/test1/+page.svelte
+++ src/routes/test1/+page.svelte
@@ -1,6 +1,89 @@
 <script>
-    import test from "../../lib/utils/test"
-    test()
+	import test from '../../lib/utils/test';
+	test();
+
+	$: innerWidth = 0;
+	$: innerHeight = 0;
+	$: outerWidth = 0;
+	$: outerHeight = 0;
+	$: scrollX = 0;
+	$: scrollY = 0;
+	$: devicePixelRatio = 0;
 </script>
 
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae eum vitae tempora nam molestiae aliquam, ad excepturi omnis pariatur aperiam tenetur commodi nemo sit. Recusandae dolor rerum veniam a iusto.</p>
+<svelte:window
+	bind:innerWidth
+	bind:innerHeight
+	bind:outerWidth
+	bind:outerHeight
+	bind:scrollX
+	bind:scrollY
+	bind:devicePixelRatio
+/>
+<p>innerWidth: {innerWidth}</p>
+<p>innerHeight: {innerHeight}</p>
+<p>outerWidth: {outerWidth}</p>
+<p>outerHeight: {outerHeight}</p>
+<p>scrollX: {scrollX}</p>
+<p>scrollY: {scrollY}</p>
+<p>devicePixelRatio: {devicePixelRatio}</p>
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae eum vitae tempora nam
+	molestiae aliquam, ad excepturi omnis pariatur aperiam tenetur commodi nemo sit. Recusandae dolor
+	rerum veniam a iusto.
+</p>
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae eum vitae tempora nam
+	molestiae aliquam, ad excepturi omnis pariatur aperiam tenetur commodi nemo sit. Recusandae dolor
+	rerum veniam a iusto.
+</p>
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae eum vitae tempora nam
+	molestiae aliquam, ad excepturi omnis pariatur aperiam tenetur commodi nemo sit. Recusandae dolor
+	rerum veniam a iusto.
+</p>
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae eum vitae tempora nam
+	molestiae aliquam, ad excepturi omnis pariatur aperiam tenetur commodi nemo sit. Recusandae dolor
+	rerum veniam a iusto.
+</p>
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae eum vitae tempora nam
+	molestiae aliquam, ad excepturi omnis pariatur aperiam tenetur commodi nemo sit. Recusandae dolor
+	rerum veniam a iusto.
+</p>
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae eum vitae tempora nam
+	molestiae aliquam, ad excepturi omnis pariatur aperiam tenetur commodi nemo sit. Recusandae dolor
+	rerum veniam a iusto.
+</p>
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae eum vitae tempora nam
+	molestiae aliquam, ad excepturi omnis pariatur aperiam tenetur commodi nemo sit. Recusandae dolor
+	rerum veniam a iusto.
+</p>
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae eum vitae tempora nam
+	molestiae aliquam, ad excepturi omnis pariatur aperiam tenetur commodi nemo sit. Recusandae dolor
+	rerum veniam a iusto.
+</p>
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae eum vitae tempora nam
+	molestiae aliquam, ad excepturi omnis pariatur aperiam tenetur commodi nemo sit. Recusandae dolor
+	rerum veniam a iusto.
+</p>
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae eum vitae tempora nam
+	molestiae aliquam, ad excepturi omnis pariatur aperiam tenetur commodi nemo sit. Recusandae dolor
+	rerum veniam a iusto.
+</p>
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae eum vitae tempora nam
+	molestiae aliquam, ad excepturi omnis pariatur aperiam tenetur commodi nemo sit. Recusandae dolor
+	rerum veniam a iusto.
+</p>
+<p style="min-width: 2000px;">
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae eum vitae tempora nam
+	molestiae aliquam, ad excepturi omnis pariatur aperiam tenetur commodi nemo sit. Recusandae dolor
+	rerum veniam a iusto.
+</p>
